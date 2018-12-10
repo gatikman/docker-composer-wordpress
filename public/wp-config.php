1a1aece2f1dc881bc1457128911a3243ea1def3b
gatikman <gaticaalex@gmail.com>
2018-12-10 11:55:39 +0100

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -83,7 +83,7 @@ define( 'WP_ALLOW_MULTISITE', true );
 
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', '35.158.236.53');
+define('DOMAIN_CURRENT_SITE', '18.185.60.131');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
